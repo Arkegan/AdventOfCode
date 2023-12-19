1ab84b30f8cd063cbd7b29bf8c18d04ab541a5a6
Ark <ark.aionfr@gmail.com>
2023-12-19 10:40:20 +0100

    Update Day5.cs

--- AOC2023/Day5/Day5.cs
+++ AOC2023/Day5/Day5.cs
@@ -1,4 +1,5 @@
-﻿
+
+using System.Diagnostics;
 using Xunit.Abstractions;
 
 namespace AOC2023.Day5;
@@ -74,24 +75,24 @@ public class Day5 : Day
         lines.Clear();
         inputData.ReadLine();
 
-        seeds = seeds.Chunk(2).SelectMany(s =>
+        var seedRange = seeds.Chunk(2).Select(s =>
         {
             var start = s[0];
-            var end = s[1];
-            return Enumerable.Range((int)start, (int)Math.Abs(end - start));
-        }).Select(s => (long)s).ToList();
+            var range = s[1];
+            return new SeedRange(start, start + range - 1);
+        }).OrderBy(r => r.Start).ToList();
 
+        var test = ssMapper.Convert(seedRange.First()).ToList();
 
-
-        var r = seeds.Select(s => ssMapper.Convert(s));
-        r = r.Select(s => sfMapper.Convert(s));
-        r = r.Select(s => fwMapper.Convert(s));
-        r = r.Select(s => wlMapper.Convert(s));
-        r = r.Select(s => ltMapper.Convert(s));
-        r = r.Select(s => thMapper.Convert(s));
-        r = r.Select(s => hlMapper.Convert(s));
+        var r = seedRange.SelectMany(s => ssMapper.Convert(s)).ToList();
+        r = r.SelectMany(s => sfMapper.Convert(s)).ToList();
+        r = r.SelectMany(s => fwMapper.Convert(s)).ToList();
+        r = r.SelectMany(s => wlMapper.Convert(s)).ToList();
+        r = r.SelectMany(s => ltMapper.Convert(s)).ToList();
+        r = r.SelectMany(s => thMapper.Convert(s)).ToList();
+        r = r.SelectMany(s => hlMapper.Convert(s)).ToList();
             
-        return r.Min().ToString();
+        return r.Min(r => r.Start).ToString();
     }
 
     private enum MapperType
@@ -103,6 +104,21 @@ public class Day5 : Day
         LightToTemperature,
         TemperatureToHumidity,
         HumidityToLocation
+    }
+
+    private class SeedRange
+    {
+        public SeedRange(long start, long end)
+        {
+            Start = start;
+            End = end;
+
+            if(Start > End)
+                Debugger.Break();
+        }
+
+        public long Start { get; set; }
+        public long End { get; set; }
     }
 
     private class Mapper
@@ -118,6 +134,8 @@ public class Day5 : Day
                 var s = item.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(a => long.Parse(a)).ToList();
                 Inner.Add(new MapperInner { SourceRangeStart = s[0], DestRangeStart = s[1], RangeLength = s[2] });
             }
+
+            Inner = Inner.OrderBy(i => i.DestRangeStart).ToList();
         }
 
         public long Convert(long n)
@@ -134,11 +152,52 @@ public class Day5 : Day
         }
 
 
+
+        public IEnumerable<SeedRange> Convert(SeedRange n)
+        {
+            var start = n.Start;
+            foreach (var inner in Inner)
+            {
+                if (start > inner.DestRangeEnd)
+                    continue;
+                if (n.End < inner.DestRangeStart)
+                    break;
+
+                if (inner.DestRangeStart > start)
+                {
+                    yield return new SeedRange(start, inner.DestRangeStart - 1);
+                    start = inner.DestRangeStart;
+                }
+
+                var end = Math.Min(n.End, inner.DestRangeEnd);
+                yield return new SeedRange(inner.GetNewPos(start), inner.GetNewPos(end));
+
+                if (end == n.End)
+                    yield break;
+                start = end + 1;
+                
+            }
+
+            yield return new SeedRange(start, n.End);
+        }
+
         public class MapperInner
         {
             public long DestRangeStart { get; set; }
+            public long DestRangeEnd => DestRangeStart + RangeLength - 1;
             public long SourceRangeStart { get; set; }
             public long RangeLength { get; set; }
+
+            public long GetNewPos(long n)
+            {
+                if (n >= DestRangeStart && n < DestRangeStart + RangeLength)
+                {
+                    var delta = n - DestRangeStart;
+                    return SourceRangeStart + delta;
+                }
+
+                return n;
+            }
         }
     }
 }
